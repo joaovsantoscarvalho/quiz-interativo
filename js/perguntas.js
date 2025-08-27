e8d39f3ed364532ca1213ed25683fbea564932a1
joaovsantoscarvalho <carvalho.joao08@escola.pr.gov.br>
2025-08-27 07:25:35 -0300

    .

--- js/perguntas.js
+++ js/perguntas.js
@@ -1,1 +1,21 @@
-
+const perguntas = [
+    {
+        enunciado: "Pergunta 1",
+        alternativas: [
+            {
+                Texto: "afirmativa 1",
+                afirmacao: [
+                    "afirmação 1",
+                    "afirmação 2"
+                ],
+            },
+                        {
+                Texto: "afirmativa 2",
+                afirmacao: [
+                    "afirmação 1,"
+                    "afirmação 2"
+                ],
+            }
+        ]
+    }
+]
