3b6bac28c9f053337405a942af37779c89cd3d03
joaovsantoscarvalho <carvalho.joao08@escola.pr.gov.br>
2025-08-27 07:36:49 -0300

    .

--- js/perguntas.js
+++ js/perguntas.js
@@ -36,5 +36,62 @@ const perguntas = [
                 ],
             }
         ]
+    },
+    {
+        enunciado: "Pergunta 3",
+        alternativas: [
+            {
+                Texto: "alternativa 1",
+                afirmacao: [
+                    "afirmação 1",
+                    "afirmação 2"
+                ],
+            },
+                        {
+                Texto: "alternativa 2",
+                afirmacao: [
+                    "afirmação 1",
+                    "afirmação 2"
+                ],
+            }
+        ]
+    },
+    {
+        enunciado: "Pergunta 4",
+        alternativas: [
+            {
+                Texto: "alternativa 1",
+                afirmacao: [
+                    "afirmação 1",
+                    "afirmação 2"
+                ],
+            },
+                        {
+                Texto: "alternativa 2",
+                afirmacao: [
+                    "afirmação 1",
+                    "afirmação 2"
+                ],
+            }
+        ]
+    },
+    {
+        enunciado: "Pergunta 5",
+        alternativas: [
+            {
+                Texto: "alternativa 1",
+                afirmacao: [
+                    "afirmação 1",
+                    "afirmação 2"
+                ],
+            },
+                        {
+                Texto: "alternativa 2",
+                afirmacao: [
+                    "afirmação 1",
+                    "afirmação 2"
+                ],
+            }
+        ]
     }
 ]
