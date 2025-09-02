f2b26d94a2ca49c647831481e29701c1df105c28
joaovsantoscarvalho <carvalho.joao08@escola.pr.gov.br>
2025-09-02 11:04:33 -0300

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,1 +1,25 @@
+import {aleatorio} from './aleatorio.js';
+import {perguntas} from './perguntas.js';
 
+const caixaPrincipal = document.querySelector(".caixa-principal");
+const caixaPerguntas = document.querySelctor(".caixa-perguntas");
+const caixaPerguntas = document.querySelctor(".caixa-alternativas");
+const caixaPerguntas = document.querySelctor(".caixa-resultado");
+const botaoIniciar = document.querySelector(".iniciar-btn");
+const telaInicial = document.querySelector(".tela-inicial");
+
+let atual = 0;
+let perguntaAtual;
+
+botaoIniciar.addEventListener('click', iniciarJogo);
+
+function iniciarJogo() {
+  atual = 0;
+  telaInicial.style.display = 'none';
+  caixaPreguntas.classList.remove(".mostrar");
+  caixaAlternativas.calssList.remove(".mostrar");
+  caixaResultado.classlist.remove(".mostrar");
+  mostraPergunta();
+}
+
+function mostraPergunta() {};
