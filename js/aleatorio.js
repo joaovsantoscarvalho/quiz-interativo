17292a2ebb2faff79ec625a4758adc1e90977386
joaovsantoscarvalho <carvalho.joao08@escola.pr.gov.br>
2025-09-10 07:38:39 -0300

    .

--- js/aleatorio.js
+++ js/aleatorio.js
@@ -1,5 +1,8 @@
-const nomes = ["claudia", "Jimmy", "kiplimo", "Hidalgo", ]
+const nomes = ["claudia", "Jimmy", "kiplimo", "Hidalgo", ];
 
-function aleatorio(lista){
-    const
+export function aleatorio(lista){
+    const posicao = Math.floor(Math.random() lista.length);
+    return lista[posicao];
 }
+
+export const nome = aleatorio(nomes)
