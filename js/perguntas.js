5c5fa7841b5ace745078ae7bf6124a75c7d3501b
joaovsantoscarvalho <carvalho.joao08@escola.pr.gov.br>
2025-09-17 08:31:36 -0300

    .

--- js/perguntas.js
+++ js/perguntas.js
@@ -1,6 +1,6 @@
 export const perguntas = [
     {
-        enunciado: "Você prefere correr uma ultramaratona ou participar de um Ironman?",
+        enunciado: "Você sabe mais sobre ultramaratona ou Ironman?",
         alternativas: [
             {
                 texto: "Ultramaratona 110km na montanha",
@@ -21,6 +21,67 @@ export const perguntas = [
         ]
     },
     {
+        enunciado: "Entre ultramaratona e Ironman, qual você acha mais desafiador?",
+        alternativas: [
+            {
+                texto: "Ultramaratona: 110km na montanha",
+                afirmacao: [
+                    "É uma prova mental e fisicamente desgastante, onde a resistência e a gestão de energia são essenciais.",
+                    "A dificuldade está na duração prolongada e nas condições extremas do terreno."
+                ],
+                proxima: 2,
+            },
+            {
+                texto: "Ironman: 3.800m nadando, 180km pedalando e 42,195km correndo",
+                afirmacao: [
+                    "O Ironman é uma prova que desafia não apenas a resistência, mas também exige habilidades técnicas nas três modalidades.",
+                    "A combinação de três esportes exige estratégias de treinamento bem estruturadas."
+                ],
+                proxima: 2,
+            }
+        ]
+    },
+    {
+        enunciado: "Qual dos dois exige mais foco em nutrição durante a prova?",
+        alternativas: [
+            {
+                texto: "Ultramaratona",
+                afirmacao: [
+                    "Correto! Manter uma boa hidratação e alimentação em intervalos regulares é essencial para a resistência durante a ultramaratona.",
+                    "A falta de nutrição adequada pode resultar em fadiga extrema, prejudicando a performance."
+                ],
+                proxima: 3,
+            },
+            {
+                texto: "Ironman",
+                afirmacao: [
+                    "Errado. Embora a nutrição também seja importante no Ironman, a ultramaratona demanda mais foco em estratégias de alimentação e hidratação para garantir o desempenho a longo prazo."
+                ],
+                proxima: 3,
+            }
+        ]
+    },
+    {
+        enunciado: "Para a recuperação após uma ultramaratona, qual estratégia é mais importante?",
+        alternativas: [
+            {
+                texto: "Alongamento e descanso ativo",
+                afirmacao: [
+                    "Correto! O descanso ativo e o alongamento ajudam na recuperação muscular após uma ultramaratona.",
+                    "É fundamental dar ao corpo tempo para se recuperar e evitar lesões durante as próximas corridas."
+                ],
+                proxima: 4,
+            },
+            {
+                texto: "Alimentação com foco em proteínas",
+                afirmacao: [
+                    "Errado. Embora as proteínas sejam importantes para a recuperação muscular, o foco principal após uma ultramaratona deve ser hidratação e descanso."
+                ],
+                proxima: 4,
+            }
+        ]
+    },
+    {
         enunciado: "Dentro do Ironman, qual parte você acha mais desafiadora?",
         alternativas: [
             {
@@ -29,75 +90,13 @@ export const perguntas = [
                     "Correto! A natação no Ironman é exigente, pois é em águas abertas, onde as condições podem ser imprevisíveis.",
                     "Essa etapa requer um bom preparo técnico e psicológico para lidar com o estresse da água."
                 ],
-                proxima: 2,
+                proxima: 5,
             },
             {
                 texto: "O ciclismo: 180km pedalando",
                 afirmacao: [
                     "Errado. Embora o ciclismo também seja desafiador, a natação é geralmente considerada a parte mais difícil para muitos atletas.",
                     "O ciclismo no Ironman exige resistência muscular e boa estratégia de alimentação e hidratação."
-                ],
-                proxima: 2,
-            }
-        ]
-    },
-    {
-        enunciado: "Para uma ultramaratona, o que é mais importante para o desempenho?",
-        alternativas: [
-            {
-                texto: "Estratégia de pacing e hidratação constante",
-                afirmacao: [
-                    "Correto! Controlar o ritmo e manter a hidratação são cruciais para concluir uma ultramaratona.",
-                    "Muitos corredores falham em controlar o ritmo no início e pagam o preço mais tarde, por isso é importante gerenciar bem a energia."
-                ],
-                proxima: 3,
-            },
-            {
-                texto: "Equipamentos super leves e de última geração",
-                afirmacao: [
-                    "Errado. Embora equipamentos bons ajudem, o fator decisivo na ultramaratona é o preparo físico e mental, junto com uma boa estratégia de corrida e alimentação."
-                ],
-                proxima: 3,
-            }
-        ]
-    },
-    {
-        enunciado: "Qual é a principal estratégia de nutrição para um triatleta durante um Ironman?",
-        alternativas: [
-            {
-                texto: "Repor carboidratos e eletrólitos durante a prova",
-                afirmacao: [
-                    "Correto! Manter os níveis de carboidratos e eletrólitos é fundamental para a performance em uma prova longa como o Ironman.",
-                    "A ingestão adequada de carboidratos e eletrólitos ajuda a prevenir a fadiga e manter a energia durante as horas de esforço."
-                ],
-                proxima: 4,
-            },
-            {
-                texto: "Comer alimentos ricos em proteínas antes e durante a prova",
-                afirmacao: [
-                    "Errado. As proteínas são importantes para recuperação, mas durante a prova, o foco deve ser nos carboidratos e eletrólitos para fornecer energia rápida.",
-                    "Durante o Ironman, a digestão de proteínas pode ser mais lenta e não fornece a energia necessária de imediato."
-                ],
-                proxima: 4,
-            }
-        ]
-    },
-    {
-        enunciado: "Você sabe o que é necessário para um atleta se preparar para uma ultramaratona?",
-        alternativas: [
-            {
-                texto: "Treinamento longo e progressivo, focando resistência",
-                afirmacao: [
-                    "Correto! A chave para uma ultramaratona é o treinamento progressivo, aumentando a distância ao longo do tempo.",
-                    "O foco deve ser resistência e adaptação do corpo a longos períodos de esforço, além de treinos específicos para o terreno."
-                ],
-                proxima: 5,
-            },
-            {
-                texto: "Treinamento curto e intenso, focando velocidade",
-                afirmacao: [
-                    "Errado. Para ultramaratonas, a resistência e o volume de treino são mais importantes do que a velocidade.",
-                    "Treinos curtos e rápidos podem ser úteis, mas o preparo para uma prova tão longa exige resistência, não velocidade."
                 ],
                 proxima: 5,
             }
